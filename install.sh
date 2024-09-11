984c2487ade625201d33a47133ce354e45f8353c
Vincent Blais <vincent.blais@webilio.ca>
2024-09-11 16:59:15 -0400

    Always install aliases

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 if [ ! -f ~/.wa_aliases ]; then
   touch ~/.wa_aliases
   echo "if [ -f ~/.wa_aliases ]; then
       . ~/.wa_aliases
   fi" >> ~/.bashrc
-
-  curl -fsSL https://raw.githubusercontent.com/webilio-xyz/webilio-commands/main/wa_aliases >> ~/.wa_aliases
 fi
+
+curl -fsSL https://raw.githubusercontent.com/webilio-xyz/webilio-commands/main/wa_aliases > ~/.wa_aliases
